936c67c3f41189523a823652d8473500f4fe6a46
jakeogh <github.com@v6y.net>
2017-03-20 19:26:54 -0700

    still testing

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pyvit',
     version='0.1.2',
-    packages=find_packages(exclude=('tests',)),
+    packages=find_packages(exclude=('test',)),
     scripts=[],
 
     install_requires=[
